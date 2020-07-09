bf4e35876b6d412c48b08eda0b16a604ed730176
Dzmitry Tsebruk <mikhama@tut.by>
2020-07-09 12:27:21 +0300

    feat: [11-124] learn about bindActionCreators()

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, bindActionCreators } from 'redux';
 
 import reducer from './reducer';
-import { inc, dec, rnd } from './actions';
+import * as actions from './actions';
 
 import 'bootstrap/dist/css/bootstrap.css';
 
 const store = createStore(reducer);
+const { dispatch } = store;
+
+const {
+  inc,
+  dec,
+  rnd,
+} = bindActionCreators(actions, dispatch);
 
 const update = () => {
   document
@@ -17,19 +24,15 @@ store.subscribe(update);
 
 document
   .getElementById('inc')
-  .addEventListener('click', () => {
-    store.dispatch(inc());
-  });
+  .addEventListener('click', inc);
 
 document
   .getElementById('dec')
-  .addEventListener('click', () => {
-    store.dispatch(dec());
-  });
+  .addEventListener('click', dec);
 
 document
   .getElementById('rnd')
   .addEventListener('click', () => {
     const payload = Math.floor(Math.random() * 10);
-    store.dispatch(rnd(payload));
+    rnd(payload);
   });
