a20d3d0609c3b0b4ae1a52f84a8580acbf29534e
Dzmitry Tsebruk <mikhama@tut.by>
2020-07-09 12:10:26 +0300

    feat: [11-121] learn about action.payload

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ const reducer = (state = 0, action) => {
     case 'DEC': {
       return state - 1;
     }
+    case 'RND': {
+      return state + action.payload;
+    }
     default:
       return state;
   }
@@ -35,4 +38,11 @@ document
   .getElementById('dec')
   .addEventListener('click', () => {
     store.dispatch({ type: 'DEC' });
+  });
+
+document
+  .getElementById('rnd')
+  .addEventListener('click', () => {
+    const payload = Math.floor(Math.random() * 10);
+    store.dispatch({ type: 'RND', payload });
   });
